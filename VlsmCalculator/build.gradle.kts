a9a7fc6228960768edfc9b9b05be63a303758631
Elhoucine Ayoub <ayoub@Ayoubs-MacBook-Pro.local>
2024-01-20 23:22:33 +0100

    add POM info details

--- VlsmCalculator/build.gradle.kts
+++ VlsmCalculator/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.vlsm.vlsmcalculator"
+    namespace = "com.ayoubelhoucine.vlsmcalculator"
     compileSdk = 34
 
     defaultConfig {
@@ -38,10 +38,12 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("com.google.android.material:material:1.11.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
+apply(from = "../gradle-mvn-push.gradle")
