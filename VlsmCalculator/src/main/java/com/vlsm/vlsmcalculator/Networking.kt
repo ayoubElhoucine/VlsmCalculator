98d3841ca9f1729af11bbc95b0ec7753a77977d6
apple <ayoubelhoucine@gmail.com>
2024-01-22 12:46:21 +0100

    clean code

--- VlsmCalculator/src/main/java/com/vlsm/vlsmcalculator/Networking.kt
+++ VlsmCalculator/src/main/java/com/vlsm/vlsmcalculator/Networking.kt
@@ -3,14 +3,14 @@ package com.vlsm.vlsmcalculator
 import com.vlsm.vlsmcalculator.model.Subnet
 
 
-public class Networking {
+class Networking {
 
     companion object {
         private val INSTANCE: Networking = Networking()
         public fun getInstance() = INSTANCE
     }
 
-    public fun calculateVlsm(
+    fun calculateVlsm(
         ipAddress: String,
         hostNumbers: HashMap<String, Int>,
     ): List<Subnet> {
