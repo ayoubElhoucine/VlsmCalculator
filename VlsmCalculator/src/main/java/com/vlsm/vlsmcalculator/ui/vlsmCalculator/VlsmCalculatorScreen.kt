4e32d5a47befed3f29892d68e981ebd0cd72ffd0
apple <ayoubelhoucine@gmail.com>
2024-01-24 12:10:52 +0100

    working on vlsm screen ui 15%

--- VlsmCalculator/src/main/java/com/vlsm/vlsmcalculator/ui/vlsmCalculator/VlsmCalculatorScreen.kt
+++ VlsmCalculator/src/main/java/com/vlsm/vlsmcalculator/ui/vlsmCalculator/VlsmCalculatorScreen.kt
@@ -2,16 +2,22 @@ package com.vlsm.vlsmcalculator.ui.vlsmCalculator
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.vlsm.vlsmcalculator.ui.components.ScreenLayout
@@ -21,12 +27,12 @@ internal fun VlsmCalculatorScreen(
     state: VlsmCalculatorState = rememberVlsmCalculatorState()
 ) {
     ScreenLayout {
-        TextField(
+        OutlinedTextField(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = 36.dp),
             value = state.ipAddress.value,
-            placeholder = { Text(text = "IP address") },
+            placeholder = { Text(text = "Enter IP address") },
             maxLines = 1,
             singleLine = true,
             onValueChange = {
@@ -37,15 +43,17 @@ internal fun VlsmCalculatorScreen(
                 imeAction = ImeAction.Done,
                 autoCorrect = false,
             ),
-            leadingIcon = {
-                Icon(imageVector = Icons.Outlined.Person, contentDescription = null)
-            }
+            shape = RoundedCornerShape(12.dp),
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = Color.White,
+                unfocusedContainerColor = Color.White,
+            ),
         )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun VlsmCalculatorScreenpRreview() {
+fun Preview() {
     VlsmCalculatorScreen()
 }
