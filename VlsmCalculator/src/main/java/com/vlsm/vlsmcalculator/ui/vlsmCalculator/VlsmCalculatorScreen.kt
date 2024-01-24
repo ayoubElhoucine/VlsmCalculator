56ce683a8bcb21824cd5969b754717cdd0199583
apple <ayoubelhoucine@gmail.com>
2024-01-24 12:16:49 +0100

    working on vlsm screen ui 20%

--- VlsmCalculator/src/main/java/com/vlsm/vlsmcalculator/ui/vlsmCalculator/VlsmCalculatorScreen.kt
+++ VlsmCalculator/src/main/java/com/vlsm/vlsmcalculator/ui/vlsmCalculator/VlsmCalculatorScreen.kt
@@ -4,15 +4,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Person
-import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
@@ -20,6 +16,7 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.vlsm.vlsmcalculator.ui.components.ScreenLayout
 
 @Composable
@@ -32,7 +29,14 @@ internal fun VlsmCalculatorScreen(
                 .fillMaxWidth()
                 .padding(horizontal = 36.dp),
             value = state.ipAddress.value,
-            placeholder = { Text(text = "Enter IP address") },
+            placeholder = {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = "IP address",
+                    textAlign = TextAlign.Center,
+                    fontSize = 16.sp,
+                )
+            },
             maxLines = 1,
             singleLine = true,
             onValueChange = {
@@ -48,6 +52,7 @@ internal fun VlsmCalculatorScreen(
                 focusedContainerColor = Color.White,
                 unfocusedContainerColor = Color.White,
             ),
+            textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.Center),
         )
     }
 }
