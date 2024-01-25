95a66cc2369ea67a3cdfea4caa8ad3404913fd78
apple <ayoubelhoucine@gmail.com>
2024-01-25 15:54:26 +0100

    working on vlsm screen ui 65%

--- VlsmCalculator/src/main/java/com/vlsm/vlsmcalculator/ui/components/MyButton.kt
+++ VlsmCalculator/src/main/java/com/vlsm/vlsmcalculator/ui/components/MyButton.kt
@@ -2,6 +2,7 @@ package com.vlsm.vlsmcalculator.ui.components
 
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -12,13 +13,14 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 internal fun MyButton(
+    modifier: Modifier = Modifier,
     onClick: () -> Unit,
     enabled: Boolean = true,
     color: Color = Color.DarkGray,
     content: @Composable RowScope.() -> Unit
 ) {
     Button(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = modifier.fillMaxWidth().height(45.dp),
         onClick = onClick,
         enabled = enabled,
         shape = RoundedCornerShape(10.dp),
